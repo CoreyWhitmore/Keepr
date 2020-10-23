744cb564e75a5cb98f749277d4e49aefc991018d
CoreyWhitmore <cjwhit97@hotmail.com>
2020-10-23 14:37:05 -0600

    SQL added missing commas

--- Setup.sql
+++ Setup.sql
@@ -1,60 +1,63 @@
-CREATE TABLE profiles(
-    id VARCHAR(255) NOT NULL,
-    name VARCHAR(255),
-    email VARCHAR(255) NOT NULL,
-    picture VARCHAR(255),
-    PRIMARY KEY (id)
-);
 
-CREATE TABLE keeps(
-    id INT NOT NULL AUTO_INCREMENT,
-    creatorId VARCHAR(255),
-    name VARCHAR(255),
-    description VARCHAR(255),
-    img VARCHAR(255),
-    views INT,
-    shares INT,
-    keeps INT,
 
-    PRIMARY KEY (id)
+-- Initial Tables Setup-----------------------------------------------------------------------
+-- CREATE TABLE profiles(
+--     id VARCHAR(255) NOT NULL,
+--     name VARCHAR(255),
+--     email VARCHAR(255) NOT NULL,
+--     picture VARCHAR(255),
+--     PRIMARY KEY (id)
+-- );
 
-    FOREIGN KEY (creatorId)
-    REFERENCES profiles (id)
-    ON DELETE CASCADE
-);
+-- CREATE TABLE keeps(
+--     id INT NOT NULL AUTO_INCREMENT,
+--     creatorId VARCHAR(255),
+--     name VARCHAR(255),
+--     description VARCHAR(255),
+--     img VARCHAR(255),
+--     views INT,
+--     shares INT,
+--     keeps INT,
 
-CREATE TABLE vaults(
-    id INT NOT NULL AUTO_INCREMENT,
-    creatorId VARCHAR(255),
-    name VARCHAR(255),
-    description VARCHAR(255),
-    isPrivate TINYINT,
+--     PRIMARY KEY (id),
 
-    PRIMARY KEY (id)
+--     FOREIGN KEY (creatorId)
+--     REFERENCES profiles (id)
+--     ON DELETE CASCADE
+-- );
 
-    FOREIGN KEY (creatorId)
-    REFERENCES profiles (id)
-    ON DELETE CASCADE
-);
+-- CREATE TABLE vaults(
+--     id INT NOT NULL AUTO_INCREMENT,
+--     creatorId VARCHAR(255),
+--     name VARCHAR(255),
+--     description VARCHAR(255),
+--     isPrivate TINYINT,
 
-CREATE TABLE vaultkeeps(
-    id INT NOT NULL AUTO_INCREMENT,
-    creatorId VARCHAR(255),
-    vaultId VARCHAR(255),
-    keepId VARCHAR(255),
+--     PRIMARY KEY (id),
+
+--     FOREIGN KEY (creatorId)
+--     REFERENCES profiles (id)
+--     ON DELETE CASCADE
+-- );
+
+-- CREATE TABLE vaultkeeps(
+--     id INT NOT NULL AUTO_INCREMENT,
+--     creatorId VARCHAR(255),
+--     vaultId VARCHAR(255),
+--     keepId VARCHAR(255),
     
 
-    PRIMARY KEY (id)
+--     PRIMARY KEY (id),
 
-    FOREIGN KEY (creatorId)
-    REFERENCES profiles (id)
-    ON DELETE CASCADE
+--     FOREIGN KEY (creatorId)
+--     REFERENCES profiles (id)
+--     ON DELETE CASCADE,
     
-    FOREIGN KEY (vaultId)
-    REFERENCES vaults (id)
-    ON DELETE CASCADE
+--     FOREIGN KEY (vaultId)
+--     REFERENCES vaults (id)
+--     ON DELETE CASCADE,
 
-    FOREIGN KEY (keepId)
-    REFERENCES keeps (id)
-    ON DELETE CASCADE
-);
+--     FOREIGN KEY (keepId)
+--     REFERENCES keeps (id)
+--     ON DELETE CASCADE
+-- );
